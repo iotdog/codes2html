8a6d9a4ccc22ae4e56d0c57165c2ce50a50d3a1b
River Wang <jiang.w@vip.163.com>
2019-05-18 18:44:59 +0800

    Update codes2html.py
    * 增加去掉注释的功能
    * 增加微信小程序wxml和wxss文件处理
    * 格式化代码

--- codes2html.py
+++ codes2html.py
@@ -12,14 +12,21 @@ from pygments.lexers import get_lexer_for_filename
 from pygments.formatters.html import DOC_HEADER
 from pygments.formatters.html import DOC_FOOTER
 
+
 def _parse_args():
-    parser = argparse.ArgumentParser(description='A tool to collect codes and highlight syntax in a single html document.')
-    parser.add_argument('sources', metavar='source', nargs='+', type=str, help='source code directory or file')
-    parser.add_argument('-e', '--extensions', help='file extensions to be considered as source files. separated with comma or a single "*" indicates all. e.g. "c,cpp,h,m,mm". default is "*"', default='*')
+    parser = argparse.ArgumentParser(
+        description='A tool to collect codes and highlight syntax in a single html document.')
+    parser.add_argument('sources', metavar='source', nargs='+',
+                        type=str, help='source code directory or file')
+    parser.add_argument(
+        '-e', '--extensions', help='file extensions to be considered as source files. separated with comma or a single "*" indicates all. e.g. "c,cpp,h,m,mm". default is "*"', default='*')
     parser.add_argument('-l', '--lines', help='limit the lines of source codes. but the content of a file is always complete, so the final lines may exceed this value. 0 for unlimited. default is 3500', type=int, default='3500')
-    parser.add_argument('-o', '--output',  help='output file path. default is output.html', default='output.html')
-    parser.add_argument('-i', '--ignore', help='path of the ignore file, similar to .gitignore. default is ignore.txt', default='ignore.txt', dest='ignore_file')
-    parser.add_argument('-f', '--footer', help='file footer string, you can use </br> to insert lines. default is </br>', default='</br>', dest='file_footer')
+    parser.add_argument(
+        '-o', '--output',  help='output file path. default is output.html', default='output.html')
+    parser.add_argument('-i', '--ignore', help='path of the ignore file, similar to .gitignore. default is ignore.txt',
+                        default='ignore.txt', dest='ignore_file')
+    parser.add_argument('-f', '--footer', help='file footer string, you can use </br> to insert lines. default is </br>',
+                        default='</br>', dest='file_footer')
     args = parser.parse_args()
 
     sources = []
@@ -32,7 +39,7 @@ def _parse_args():
     if len(sources) != len(args.sources):
         return None
     args.sources = sources
-    
+
     args.output = os.path.expanduser(args.output)
 
     args.ignore_file = os.path.expanduser(args.ignore_file)
@@ -44,6 +51,7 @@ def _parse_args():
         args.lines = 2 ** 31
 
     return args
+
 
 def _parse_ignore_file(path):
     if not os.path.exists(path):
@@ -57,24 +65,28 @@ def _parse_ignore_file(path):
     except:
         return []
 
+
 def _parse_extensions(extensions_string):
     if extensions_string.strip() == '*':
         return []
     array = extensions_string.split(',')
     array = ['*.' + e.strip() for e in array if len(e.strip()) > 0]
-    array = list(set(array)) # remove duplicated values
+    array = list(set(array))  # remove duplicated values
     return array
+
 
 def _short_class_name(obj):
     t = type(obj)
     m = re.match(r'<class \'(\w+\.)*(\w+)\'>', str(t))
     return m.group(2)
 
+
 def _match_any_pattern(name, patterns):
     for pattern in patterns:
         if fnmatch.fnmatch(name, pattern):
             return True
     return False
+
 
 class Codes2HtmlTool:
     def __init__(self, args):
@@ -90,16 +102,16 @@ class Codes2HtmlTool:
 
     def _should_ignore_file(self, name):
         return _match_any_pattern(name, self.args.ignore_patterns)
-    
+
     def _accept_extension(self, name):
         patterns = self.args.extension_patterns
         return len(patterns) == 0 or _match_any_pattern(name, patterns)
 
     def _header(self):
         return DOC_HEADER % dict(
-            title = self.hf.title, 
-            styledefs = self.hf.get_style_defs('body'), 
-            encoding = self.hf.encoding)
+            title=self.hf.title,
+            styledefs=self.hf.get_style_defs('body'),
+            encoding=self.hf.encoding)
 
     def _footer(self):
         return DOC_FOOTER
@@ -121,26 +133,56 @@ class Codes2HtmlTool:
             elif self._accept_extension(subfile):
                 self._highlight_and_write_file(full_path)
 
+    def _filter_lines(self, lines):
+        new_lines = []
+        commentFlag = False
+        for line in lines:
+            tmp = line.strip()
+            if '' == tmp or tmp.startswith('//'):
+                continue
+            if tmp.startswith('/*') or tmp.startswith('<!--'):
+                commentFlag = True
+            if tmp.endswith('*/') or tmp.endswith('-->'):
+                commentFlag = False
+                continue
+            if commentFlag:
+                continue
+            idx = line.find('//') # 删除行内注释
+            if idx > 0:
+                line = line[:idx]
+            new_lines.append(line)
+        return new_lines
+
     def _highlight_and_write_file(self, full_path):
         write_fd = self.write_fd
         hf = self.hf
         footer = self.args.file_footer
         try:
+            orig_full_path = full_path
+            if full_path.endswith('.wxml'):
+                full_path = full_path.replace('.wxml', '.xml', 1)
+            if full_path.endswith('.wxss'):
+                full_path = full_path.replace('.wxss', '.css', 1)
             lexer = get_lexer_for_filename(full_path)
+            full_path = orig_full_path
             with open(full_path) as fd:
-                lines = fd.readlines()
-                self.written_lines += len(lines)
-                content = ''.join(lines)
-                if full_path.endswith('.h'):
-                    # ".h" file is possible to be objective-c.
-                    # guess again with file content to determine the actual syntax
-                    lexer = get_lexer_for_filename(full_path, code=content)
-                formatted = highlight(content, lexer, hf)
-                write_fd.write(formatted)
-                write_fd.write(footer)
-                print('highlighted with ', _short_class_name(lexer), ': "', full_path, '"', sep='')
+                    lines = self._filter_lines(fd.readlines())
+                    # 追加文件名到首行
+                    lines.insert(0, '>>>>' + os.path.basename(full_path) + '\n')
+                    self.written_lines += len(lines) - 1
+                    content = ''.join(lines)
+                    if full_path.endswith('.h'):
+                        # ".h" file is possible to be objective-c.
+                        # guess again with file content to determine the actual syntax
+                        lexer = get_lexer_for_filename(full_path, code=content)
+                    formatted = highlight(content, lexer, hf)
+                    write_fd.write(formatted)
+                    write_fd.write(footer)
+                    print('highlighted with ', _short_class_name(
+                        lexer), ': "', full_path, '"', sep='')
         except:
             print('not source code: "', full_path, '"', sep='')
+
 
 if __name__ == "__main__":
     t1 = time.time()
